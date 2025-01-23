899b36f7941067f4d45640b238fcc75027bfa97d
Ryley Watson <ryleyswatson@gmail.com>
2025-01-23 10:16:06 -0800

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const lowerCaseName = name.toLowerCase();
+    return drivers.filter( driver => driver.toLowerCase() === lowerCaseName)
+}
+
+function fuzzyMatch(drivers, letters){
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
